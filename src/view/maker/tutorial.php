772497bd89960cea15aa781678aead4727b477c0
Alexander Buysse <alexanderbuysse@MBPvanAexander2.lan>
2021-01-18 13:00:37 +0100

    header tutorial af en workin on stap 1 #28

--- src/view/maker/tutorial.php
+++ src/view/maker/tutorial.php
@@ -1,44 +1,44 @@
 <main class="container">
   <div class="tutorial-one">
     <nav>
-      <a href="index.php?page=home" class=button-home>home</a>
+      <a href="index.php?page=home" class=button-home>Home</a>
     </nav>
 
     <article class="header">
       <div class="header-background">
-        <h1 class="header__title">De diy hoverscraft <span class="header__title-bold">TUTORIAL</span></h1>
+        <h1 class="header__title d3">De diy hoverscraft <span class="header__title-bold">TUTORIAL</span></h1>
       </div>
 
       <section class="header-section">
         <h2 class="section-title">De benodigdheden</h2>
-        <div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
-          <div class="header__items"></div>
+        <div class="header__items">
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
+          <div class="header__item"></div>
         </div>
       </section>
 
-      <section class="header-section">
+      <section class="header-section header-section__ready">
         <h2 class="section-title">Ben je er klaar voor?</h2>
         <p class="section__message"></p>
-        <div>
-          <a href="">Ja!</a>
-          <a href="">Nee...</a>
+        <div class="header-section__buttons">
+          <a href="index.php?page=tutorial&step=0" class="button">Ja!</a>
+          <a href="" class="button">Nee...</a>
         </div>
       </section>
     </article>
   </div>
 
 
-  <article class="step">
-    <h2 class="step__title">Stap <span class="step__title-number">1</span></h2>
+  <article class="step-black">
+    <h2 class="section-title white-color step__title">Stap <span class="step__title-number">1</span></h2>
   </article>
 
 </main>
